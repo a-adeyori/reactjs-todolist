9557589d0c8b348a9170297ffd99873f699ab109
a-adeyori <adeyoriademide@gmail.com>
2024-12-13 01:13:21 -0600

    implemented a local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -7,8 +7,13 @@ function App() {
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState("")
 
+  function persisitData(newList){
+    localStorage.setItem('todos', JSON.stringify({todos:newList}))
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persisitData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -16,6 +21,7 @@ function App() {
     const  newTodoList = todos.filter((todo, todoIndex)=> {
       return todoIndex !== index
     })
+    persisitData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -25,6 +31,21 @@ function App() {
     handleDeleteTodo(index)
 
   }
+
+  useEffect(()=>{
+    if (!localStorage){
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+
+    if (!localTodos){
+      return 
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   return (
     
